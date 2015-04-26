583284f8ac573137552635b4512c7d845e59528a
petr-vs <pvu1vs@gmail.com>
2015-04-26 20:52:48 +0200

    pozn //

--- new.php
+++ new.php
@@ -1,10 +1,20 @@
 <?php
 //kukukuk
 require '';
-include '';
+include_once '';
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+phpinfo();
+for ($i = 0; $i < 100; $i++) {
+    sdfsdfsdf($i);
+    if(true){
+        sdfsdf();
+    }
+    else{
+        
+    }
+}
 ?>
